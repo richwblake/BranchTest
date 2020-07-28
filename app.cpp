a1e6ef1cdd6f90052eb34f2ccf440fbd9beb20b8
Wills Blake <richwblake@gmail.com>
2020-07-27 21:44:53 -0400

    Added main and reduce function

---
+++ app.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+int reduce(std::vector<int> v)
+    {
+        int sum = 0;
+        for(int number : v)
+        {
+            sum += number;
+        }
+        return sum;
+    }
+
+
+int main()
+{
+    std::vector<int> v = {1, 2, 3, 4};
+    
+    std::cout << "The sum of the values in vector [1, 2, 3, 4] = " << reduce(v) << std::endl;
+
+    return 0;
+}
